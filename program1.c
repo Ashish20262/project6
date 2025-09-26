710d6d572191f0df7104b8aaa527f1a6af24d896
Ashish20262 <ap5009010@gmail.com>
2025-09-26 09:49:24 +0530

    first commit

---
+++ program1.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+
+// int main(){
+
+//     char name[] = "nayan";
+//     printf("Enter your name = ");
+//     scanf("%d",&name);
+//     printf("\nreverse = %s",strrev(name)); 
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main(){
+    char name[] = "manish";
+    printf("reverse =%s", strrev(name));
+}
